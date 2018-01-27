d89927967b51743d0e2bf885e06d5951d3bf2f31
César Valenciano Fonseca <www.cavf.20@gmail.com>
2018-01-27 12:27:40 -0600

    rESGUARD 4.1

--- views/table.php
+++ views/table.php
@@ -69,7 +69,7 @@
 						<td>
 							<a href="index.php?vcode=<?php echo $row['code'] ?>" class="btn btn-sm btn-info" title="Modificar registro"><i class="fa fa-pencil" aria-hidden="true"></i></a>
 							<a href="controlers/deleteRegistry.php?dcode=<?php echo $row['code'] ?>" class="btn btn-sm btn-danger btn-eliminar" onclick="return confirm('Seguro de eliminar?')" title="Eliminar registro"><i class="fa fa-trash-o" aria-hidden="true"></i></a>
-							<a href="views/report.php?codeR=<?php echo $row ['code']; ?>" name="create_pdf" class="btn btn-sm btn-success" title="Carta responsiva"><i class="fa fa-file-pdf-o" aria-hidden="true"></i></a>
+							<a href="views/report.php?codeR=<?php echo $row ['code']; ?>" name="create_pdf" class="btn btn-sm btn-success" title="Carta responsiva" target="blank"><i class="fa fa-file-pdf-o" aria-hidden="true"></i></a>
 						</td>
 
 						<?php }else{ ?>
